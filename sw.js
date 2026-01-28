cd8040508c2f92ec65a5306793e2acbbcb03efb8
ejam2trader-max <ejam2.trader@gmail.com>
2026-01-28 13:26:54 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
-const CACHE = 'grace-v1';
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(['/', 'index.html', 'app.js']))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+const CACHE_NAME = 'grace-v2';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(['./', './index.html', './app.js', './manifest.json'])));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
